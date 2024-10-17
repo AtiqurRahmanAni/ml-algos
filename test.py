8a7859674c74c3b63dac29c2748787c42d8c9c9a
atiqurrahmanani <ani.atikur99@gmail.com>
2024-10-17 11:53:16 +0600

    updated

--- test.py
+++ test.py
@@ -1,2 +1,20 @@
-for i in range(100):
-    print(i)
+import os
+import torch
+import sys
+
+os.environ['HSA_OVERRIDE_GFX_VERSION'] = '10.3.0'
+os.environ['HIP_VISIBLE_DEVICES'] = '0'
+
+print(torch.cuda.get_device_name())
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
+print()
+
+print(sys.path)
+print()
+
+print(os.listdir("/opt/rocm"))
+print()
+
+x = torch.tensor([1]).to(device)
+print(x + x)
